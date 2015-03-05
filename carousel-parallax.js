de087649b9d7520c6f5d919f402aa5f1c90fbb8b
Mr21 <thomastortorini@gmail.com>
2015-03-05 20:53:04 +0100

    -1;0 left / length;length+1

--- carousel-parallax.js
+++ carousel-parallax.js
@@ -26,17 +26,18 @@ jQuery.fn.carouselParallax.obj = function(el_ctn, params) {
 
 jQuery.fn.carouselParallax.obj.prototype = {
 	slide: function(i) {
+		var dir = this.slideCurrId < i;
+
 		i = i % this.jq_slides.length;
 		if (i < 0)
 			i += this.jq_slides.length;
 
 		if (i !== this.slideCurrId) {
 			var	that = this,
-				dir = this.slideCurrId < i,
 				jq_slideA = this.jq_slides.eq(i),
 				jq_slideB = this.jq_slides.eq(this.slideCurrId),
 				progress = function(_, p) {
-					var pA, pB;
+					var pA, pB, layer;
 					if (dir) {
 						pA = 1 - p;
 						pB = -p;
@@ -45,38 +46,38 @@ jQuery.fn.carouselParallax.obj.prototype = {
 						pB = p;
 					}
 					jq_slideA[0].jq_layers.each(function() {
-						var layer = $(this);
+						layer = $(this);
 						layer.css("left", (layer.css("z-index") * pA) + "px");
 					});
 					jq_slideB[0].jq_layers.each(function() {
-						var layer = $(this);
+						layer = $(this);
 						layer.css("left", (layer.css("z-index") * pB) + "px");
 					});
 				};
 			if (this.isSliding) {
 				jq_slideA.finish();
 				jq_slideB.finish();
-				return;
-			}
-			this.isSliding = true;
-			jq_slideA
-				.css("left", dir ? "100%" : "-100%")
-				.animate({ left: "0%" }, this.dur, this.easing);
-			jq_slideB
-				.animate(
-					{
-						left: dir ? "-100%" : "100%"
-					}, {
-						duration: this.dur,
-						easing: this.easing,
-						progress: progress,
-						complete: function() {
-							progress(undefined, 1);
-							that.isSliding = false;
-							that.slideCurrId = i;
+			} else {
+				this.isSliding = true;
+				jq_slideA
+					.css("left", dir ? "100%" : "-100%")
+					.animate({ left: "0%" }, this.dur, this.easing);
+				jq_slideB
+					.animate(
+						{
+							left: dir ? "-100%" : "100%"
+						}, {
+							duration: this.dur,
+							easing: this.easing,
+							progress: progress,
+							complete: function() {
+								progress(undefined, 1);
+								that.isSliding = false;
+								that.slideCurrId = i;
+							}
 						}
-					}
-				);
+					);
+			}
 		}
 
 		return this;
